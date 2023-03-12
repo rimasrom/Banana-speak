7d30076be808e78d9cbf892ad3bc38e07add174d
rimasrom <hembromsamir715@gmail.com>
2023-03-13 01:50:44 +0530

    done backend

---
+++ app.js
@@ -0,0 +1,28 @@
+var inputText = document.querySelector('#input')
+var btnTranslate = document.querySelector("#btn-translate")
+var textOutput = document.querySelector("#output")
+
+const url = "https://api.funtranslations.com/translate/minion.json"
+
+function completeUrl(text){
+    return url + "text" + text;
+}
+
+function err(){
+    alert("something went wrong ! Try again after some time...")
+}
+
+function clickEventHandler(){
+    var input = inputText.value;
+    fetch(completeUrl(inputText))
+    .then(res =>{
+        res.json
+    })
+    .then(output =>{
+        textOutput.innerText = output.contents.translation;
+    })
+    .catch(err)
+}
+
+btnTranslate.addEventListener("click", clickEventHandler)
+
